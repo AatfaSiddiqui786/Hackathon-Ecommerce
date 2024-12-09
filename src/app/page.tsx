26bafc40a8a5d895c05f324940774d3f624e3f3d
Rafiha Siddiqui <rafihasiddiqui@gmail.com>
2024-12-09 11:33:02 -0800

    completed Hackathon

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import Productthings from "./Components/Product things";
+import Matter from "./Components/Matter";
+import Signup from "./Components/Signup";
+import { Hero } from "./Components/Hero";
+import Branded from "./Components/Branded";
+import Things from "./Components/Things";
+
+
+export default function Home() {
+  return (
+    <div>
+      
+      <Hero />
+      <Branded />
+      <Things />
+       <Productthings />
+      <Signup/>
+      <Matter/>
+
+    </div>
+  );
+}
